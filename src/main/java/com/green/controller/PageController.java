9040dfeb1c836d917cd2c439c002fb9f0114a0d6
DP <tree1787@naver.com>
2023-01-04 16:36:30 +0900

    1차병합

--- src/main/java/com/green/controller/PageController.java
+++ src/main/java/com/green/controller/PageController.java
@@ -6,9 +6,15 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class PageController {
 
+    //임시, 사원페이지로 이동
     @GetMapping("/member/main")
-    public String admin(){
-        return "layout/main";
+    public String member(){
+        return "member/main";
     }
 
+    //임시, 어드민페이지로 이동
+    @GetMapping("/admin/main")
+    public String admin(){
+        return "admin/main";
+    }
 }
