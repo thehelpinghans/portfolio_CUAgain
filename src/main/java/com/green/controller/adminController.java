5ccebb1b3f77b74d4b6d858c6c55a4e406e5eaf8
DP <tree1787@naver.com>
2023-01-06 12:44:50 +0900

    사원등록,조회/수정페이지 프론트만

--- src/main/java/com/green/controller/adminController.java
+++ src/main/java/com/green/controller/adminController.java
@@ -1,10 +1,25 @@
 package com.green.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class adminController {
 
-	
-	
+    //사원 등록 페이지이동
+    @GetMapping("/admin/emp/reg")
+    public String empReg(){
+        return "/admin/employee/reg";
+    }
+    //사원 조회/수정 페이지이동
+    @GetMapping("/admin/emp/list")
+    public String empList(){
+        return "/admin/employee/list";
+    }
+
+    //사원디테일가져오기
+    @GetMapping("/admin/member/detailTag")
+    public String empDetail(){
+        return "admin/employee/detailTag";
+    }
 }
