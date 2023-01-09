7f35d1ca43dc72dc881c6f22fa964eba0ed91775
hong-wongi <hwg5640@gmail.com>
2023-01-09 11:00:16 +0900

    0109

---
+++ src/main/java/com/green/domain/entity/ReplyEntity.java
@@ -0,0 +1,39 @@
+package com.green.domain.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "reply")
+@Entity
+public class ReplyEntity extends BaseDateEntity{
+
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Id
+    private Long id;
+
+    @Column(nullable = false)
+    private String content;
+
+    @Enumerated(EnumType.STRING)
+    private Status status;
+
+    @JoinColumn
+    @ManyToOne(fetch = FetchType.EAGER)
+    private BoardEntity board;
+
+    @JoinColumn
+    @ManyToOne(fetch = FetchType.EAGER)
+    private EmployeesEntity employees;
+
+
+
+
+}
---
+++ src/main/java/com/green/service/impl/BoardServiceProcess.java
@@ -0,0 +1,8 @@
+package com.green.service.impl;
+
+import com.green.service.BoardService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class BoardServiceProcess implements BoardService {
+}
