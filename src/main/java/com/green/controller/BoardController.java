e8c113f8c5c42a118de9f379efe405105f716857
hong-wongi <hwg5640@gmail.com>
2023-01-12 17:49:15 +0900

    0112

--- src/main/java/com/green/controller/BoardController.java
+++ src/main/java/com/green/controller/BoardController.java
@@ -1,6 +1,5 @@
 package com.green.controller;
 
-import com.green.domain.dto.BoardDetailDTO;
 import com.green.domain.dto.BoardInsertDTO;
 import com.green.domain.dto.BoardListDTO;
 import com.green.security.MyUserDetails;
@@ -12,7 +11,6 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
 
 @Controller
 public class BoardController {
@@ -20,28 +18,55 @@ public class BoardController {
     @Autowired
     private BoardService service;
 
-    //공지사항 등록후 다시 리스트페이지로 이동
+    //공지사항 등록     후 다시 리스트페이지로 이동
     @PostMapping("/admin/board/reg")
-    public String board(BoardInsertDTO dto){
-        service.save(dto,1L);
-        return "redirect:/admin/board/noticeList";
+    public String board(BoardInsertDTO dto, @AuthenticationPrincipal MyUserDetails userDetails){
+        System.out.println(">>>등록>>"+dto.getBoardType());
+        service.save(dto, userDetails.getId());
+        return "redirect:/admin/board/boardList/"+dto.getBoardType();
+    }
+//
+//    //자유게시판 등록 후 다시 리스트페이지로 이동
+//    @PostMapping("/admin/board/regfree")
+//    public String free(BoardInsertDTO dto, @AuthenticationPrincipal MyUserDetails userDetails){
+//        service.fsave(dto, userDetails.getId());
+//        return "redirect:/admin/board/freeList";
+//    }
+    //공지사항 등록페이지이동
+    @GetMapping("/admin/board/write/{type}")
+    public String write(@PathVariable String type, Model model) {
+        model.addAttribute("type",type);
+        return "admin/board/write";
     }
 
-    //공지사항 리스트페이지
-    @GetMapping("/admin/board/noticeList")
-    public String boardList(Model model){
-        service.boardList(model);
+    //리스트페이지
+    @GetMapping("/admin/board/boardList/{type}")
+    public String boardList(@PathVariable String type, Model model) {
+        service.boardList(type, model);
+
+        System.out.println(">>리스트>" + type);
+        model.addAttribute("type",type);
         return "admin/board/notice";
+
+
     }
 
-    //공지사항 리스트에서 눌렀을때 상세페이지
+    //자유게시판 리스트페이지
+//    @GetMapping("/admin/board/freeList")
+//    public String freeList(Model model){
+//        service.freeList(model);
+//        return "admin/board/free";
+//    }
+
+    //공지사항 리스트에서 목록 눌렀을때 상세페이지
     @GetMapping("/admin/board/view/{boardId}")
     public String boardListDetail(@PathVariable long boardId, Model model){
         service.boardListDetail(boardId, model);
+
         return "admin/board/view";
     }
 
-    //상세페이지에서 수정버튼눌렀을때의 페이지
+    //공지사항 상세페이지에서 수정버튼눌렀을때의 페이지
     @GetMapping("/admin/board/{boardId}")
     public String boardmodify(@PathVariable("boardId") long boardId, Model model){
         service.getBoardDetail(boardId, model);
@@ -49,10 +74,12 @@ public class BoardController {
         return "admin/board/edit";
     }
 
-    //수정완료눌렀을때의 페이지?
+    //공지사항 수정완료눌렀을때의 페이지?
     @PostMapping("/admin/board/update/{boardId}")
     public String update(@PathVariable("boardId") long boardId, BoardListDTO dto){
         service.boardUpdate(boardId, dto);
-        return "redirect:/admin/board/noticeList";
+        return "redirect:/admin/board/boardList";
     }
+
+
 }
