ef658061680056c39ea0c2bcd38541441df7aa4f
DP <tree1787@naver.com>
2023-01-19 16:20:13 +0900

    결재등록상태변경수정

--- src/main/java/com/green/service/DocumentService.java
+++ src/main/java/com/green/service/DocumentService.java
@@ -1,13 +1,29 @@
 package com.green.service;
 
+import com.green.security.MyUserDetails;
 import org.springframework.ui.Model;
 
-import com.green.domain.dto.PaymentDTO;
+import com.green.domain.dto.DocumentInsertDTO;
 
 public interface DocumentService {
 
-	void getlist(Model model);
+	void getList(Model model);
 
-	void save(PaymentDTO dto);
+	void save(DocumentInsertDTO dto);
 
+	void getMyList(Model model, MyUserDetails userDetails);
+
+	void getDetail(long docId, long name, Model model);
+
+	void getDocListBySearch(String type, String data, Model model);
+
+	void getAllList(Model model);
+
+	void checkedStatus(long docId);
+
+	void changeDocStatus(long docId, long docStatus);
+
+	void getEditTag(long docId, Model model);
+
+	void editProcess(long docId, String title, String content);
 }
