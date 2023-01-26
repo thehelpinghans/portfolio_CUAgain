ebdf9fc3fb1a3d35cb20ea05ea3a7d71b14227be
DP <tree1787@naver.com>
2023-01-26 15:22:27 +0900

    0126병합

--- src/main/java/com/green/controller/AdminController.java
+++ src/main/java/com/green/controller/AdminController.java
@@ -1,19 +1,6 @@
 package com.green.controller;
 
-import java.util.List;
-import java.util.Map;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.multipart.MultipartFile;
-
+import com.green.domain.dto.DepartmentDTO;
 import com.green.domain.dto.EmployeesDetailDTO;
 import com.green.domain.dto.EmployeesInsertDTO;
 import com.green.domain.dto.TeamDTO;
@@ -21,6 +8,14 @@ import com.green.service.DepartmentService;
 import com.green.service.EmployeesService;
 import com.green.service.SalaryService;
 import com.green.service.TeamService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.List;
+import java.util.Map;
 
 @Controller
 public class AdminController {
@@ -130,7 +125,7 @@ public class AdminController {
     //부서등록
 	@ResponseBody
 	@PostMapping("/admin/addDepart")
-	public void depratReg(DepartmentDTO dto) { 
+	public void depratReg(DepartmentDTO dto) {
 		System.out.println(dto);
 		//등록하면 dto에 업데이트! 하고 사원 리스트로 리턴
 		depService.save(dto);
